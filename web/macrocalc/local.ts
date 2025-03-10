2018efed0ba900b43ea18b5174e07f3f3e172b8d
oscar <oscar@wahltinez.org>
2025-03-10 15:29:51 +1100

    Improve search functionality

--- web/macrocalc/local.ts
+++ web/macrocalc/local.ts
@@ -1,5 +1,13 @@
 import Fuse from "fuse.js";
 
+async function fetchAndProcessProducts() {
+  const products = await fetch("products.json").then((r) => r.json());
+  return products.map((product: any) => ({
+    ...product,
+    search: `${product.name} ${product.brand}`.toLocaleLowerCase(),
+  }));
+}
+
 function parseProductLookupResult(result: any) {
   return {
     id: result.id,
@@ -23,11 +31,46 @@ function parseProductLookupResult(result: any) {
 }
 
 const fuseOptions = {
-  keys: ["name", "brand"],
+  keys: ["search"],
   ignoreDiacritics: true,
   ignoreLocation: true,
+  includeScore: true,
   threshold: 0.4,
 };
+
+function searchProducts(
+  fuse: Fuse<any & { search: string }>,
+  query: string
+): any & { search: string }[] {
+  const queryKeywords = new Set(
+    query
+      .toLowerCase()
+      .split(/\s+/)
+      .filter((word) => word.length > 1)
+  );
+  if (queryKeywords.size === 0) return [];
+
+  // Here we do the following:
+  // 1. Iterate over each of the query keywords
+  // 2. Perform fuzzy search for each keyword
+  // 3. Add up the scores for each product
+  const results = new Map();
+  for (const keyword of queryKeywords) {
+    for (const { item, score } of fuse.search(keyword)) {
+      const normScore = 1 - (score || 0);
+      if (!score) continue;
+      results.set(item.id, {
+        item,
+        score: (results.get(item.id)?.score || 0) + normScore,
+      });
+    }
+  }
+
+  // Sort the results by score and return them.
+  return Array.from(results.values())
+    .sort((a, b) => b.score - a.score)
+    .map(({ item }) => item);
+}
 
 export class Local {
   _fuse: Promise<Fuse<any>> | null;
@@ -40,7 +83,7 @@ export class Local {
 
   get products() {
     if (!this._products) {
-      this._products = fetch("products.json").then((r) => r.json());
+      this._products = fetchAndProcessProducts();
     }
     return this._products;
   }
@@ -61,9 +104,8 @@ export class Local {
 
   async searchFoods(query: string, { limit = 10 } = {}) {
     const fuse = await this.fuse;
-    return fuse
-      .search(query)
+    return searchProducts(fuse, query)
       .slice(0, limit)
-      .map((food) => parseProductLookupResult(food.item));
+      .map(parseProductLookupResult);
   }
 }
