6e4a01e43773302d85e5264442ae35d7da1f1342
oscar <oscar@wahltinez.org>
2025-03-10 14:15:14 +1100

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 import * as fs from "fs";
 
+const WEB_PREFIX = "./dist/web";
+
 export default {
   target: "web",
-  mode: "development",
+  mode: "production",
   output: {
     filename: "web/[name]",
     libraryTarget: "module",
   },
   entry: fs
-    .readdirSync("./dist/web", { withFileTypes: true })
+    .readdirSync(WEB_PREFIX, { withFileTypes: true })
     .filter((dir) => dir.isDirectory())
     .map((dir) => `${dir.name}/main.js`)
-    .filter((file) => fs.existsSync(file))
-    .concat(["/main.js"])
-    .reduce((x, f) => Object.assign(x, { [f]: `./dist/web/${f}` }), {}),
+    .filter((file) => fs.existsSync(`${WEB_PREFIX}/${file}`))
+    .concat(["main.js"])
+    .reduce((x, f) => Object.assign(x, { [f]: `${WEB_PREFIX}/${f}` }), {}),
   optimization: {
     minimize: true,
   },
